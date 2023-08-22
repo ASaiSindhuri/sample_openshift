a213aad94bf3b9a607165f583fd87c5add602d1a
ASaiSindhuri <91867738+ASaiSindhuri@users.noreply.github.com>
2023-08-22 15:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route("/how are you")
 def hello() :
-    return "I am good without docker"
+    return "I am good without docker with auto trigger"
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0", port=8080)
